50c1792c49cd1c676b7331cebfa6f533a835ee86
gabrielgamboa <gabriel.gamboa@mblabs.com.br>
2023-10-10 20:17:59 -0300

    feat: create home html

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,18 @@
 export function Home() {
-  return <h1>Página Home</h1>
+  return (
+    <div className="flex flex-col items-center justify-center w-full py-4">
+      <h1 className="text-3xl">Bem vindo(a) ao meu Linktree!</h1>
+      <span>Veja abaixo meus links 👇</span>
+
+      <main>
+        <section>
+          <a href="">
+            <p>
+              Canal do youtube
+            </p>
+          </a>
+        </section>
+      </main>
+    </div>
+  )
 }
