d206048117d6f40d4e138b9f6fb53b8a2ee116e7
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-10-11 11:37:37 -0300

    feat: social component and login page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,34 @@
+import { Social } from "../../components/Social";
+import { FaFacebook, FaYoutube, FaInstagram } from 'react-icons/fa'
+
 export function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full py-4">
-      <h1 className="text-3xl">Bem vindo(a) ao meu Linktree!</h1>
-      <span>Veja abaixo meus links 👇</span>
+      <h1 className=" text-white font-bold md:text-4xl text-3xl mt-20">Bem vindo(a) ao meu Linktree!</h1>
+      <span className="text-gray-50 mb-7 mt-6">Veja abaixo meus links 👇</span>
 
-      <main>
-        <section>
+      <main className="flex flex-col max-w-xl w-11/12">
+        <section className="bg-white w-full rounded-lg text-center mb-4 py-2 select-none transition-transform hover:scale-105">
           <a href="">
-            <p>
+            <p className="md:text-lg text-base">
               Canal do youtube
             </p>
           </a>
         </section>
+
+        <footer className="flex justify-center gap-4 my-4">
+          <Social url="https://youtube.com.br">
+            <FaFacebook size={35} color="#FFF" />
+          </Social>
+          
+          <Social url="https://youtube.com.br">
+            <FaYoutube size={35} color="#FFF" />
+          </Social>
+
+          <Social url="https://youtube.com.br">
+            <FaInstagram size={35} color="#FFF" />
+          </Social>
+        </footer>
       </main>
     </div>
   )
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,9 @@
 export function Login() {
-  return <h1>Página login</h1>
+  return (
+    <div className=" flex flex-col justify-center items-center h-screen">
+      <h1 className="text-white mb-7 font-bold text-5xl">Dev
+      <span className=" bg-gradient-to-r from-yellow-500 to-orange-700 bg-clip-text text-transparent">link</span>
+      </h1>
+    </div>
+  )
 }
