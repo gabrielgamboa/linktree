e5400511dead8cea270f9fe66350697d9bc24157
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-10-13 17:32:11 -0300

    feat: form in admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,9 +1,45 @@
+import { useState } from "react"
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 
 export function Admin() {
+  const [nameUrl, setNameUrl] = useState('')
+  const [url, setUrl] = useState('')
+  const [backgroundLinkColor, setBackgroundLinkColor] = useState('#f1f1f1')
+  const [textColor, setTextColor] = useState('#121212')
+
   return (
-    <div className="flex flex-col items-center min-h-screen">
+    <div className="flex flex-col items-center min-h-screen px-4">
       <Header/>
+      
+      <form className="mt-8 flex flex-col w-full max-w-xl ">
+        <label className="text-white my-2 font-medium" >Nome do link</label>
+        <Input
+          placeholder="Digite o nome do link..."
+          value={nameUrl}
+          onChange={(e) => setNameUrl(e.target.value)}
+        />
+
+        <label className="text-white my-2 font-medium" >URL do link</label>
+        <Input
+          placeholder="Digite a URL..."
+          type="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+
+        <section className="flex my-4 gap-5">
+          <div className="flex gap-2">
+            <label className="text-white font-medium mt-2 ">Fundo do link</label>
+            <input type="color" value={backgroundLinkColor} onChange={(e => setBackgroundLinkColor(e.target.value))} />
+          </div>
+
+          <div className="flex gap-2">
+            <label className="text-white font-medium mt-2">Cor do link</label>
+            <input type="color" value={textColor} onChange={(e => setTextColor(e.target.value))}/>
+          </div>
+        </section>
+      </form>
     </div>
   )
 }
